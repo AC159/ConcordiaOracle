1d74cc665093f23412c80bcde8fa74a4a46619fe
Anastassy Cap <anacap100@gmail.com>
2022-04-07 09:58:18 -0400

    added bottom navigation bar

--- lib/Components/HomePage/HomePage.dart
+++ lib/Components/HomePage/HomePage.dart
@@ -8,13 +8,35 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  int _selectedIndex = 0;
+
+  static const List<Widget> bottomNavigationTabs = [
+    Text('Profile'),
+    Text('Forum'),
+    Text('Ratings'),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Center(child: Text('Concordia Oracle'))),
       body: Container(
         child: Center(child: Text('Welcome to Concordia Oracle!'))
-      )
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: (int index) => {
+          setState(() => {
+            _selectedIndex = index
+          })
+        },
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+          BottomNavigationBarItem(icon: Icon(Icons.message), label: 'Forum'),
+          BottomNavigationBarItem(icon: Icon(Icons.star), label: 'Ratings')
+        ]
+      ),
     );
   }
 }
